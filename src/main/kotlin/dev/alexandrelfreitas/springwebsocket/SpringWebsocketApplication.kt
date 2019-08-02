990567d3a3cb468e817adcdc8d549e329e3894c5
Alexandre Freitas <falecom@alexandrelfreitas.dev>
2019-08-02 20:44:28 -0300

    primeiro commit

---
+++ src/main/kotlin/dev/alexandrelfreitas/springwebsocket/SpringWebsocketApplication.kt
@@ -0,0 +1,11 @@
+package dev.alexandrelfreitas.springwebsocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebsocketApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebsocketApplication>(*args)
+}
